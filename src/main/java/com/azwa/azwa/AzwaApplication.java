1b26fc2e4557e81e4d6b1abd7dada3f5fbc6b237
Azuau7 <azuau.benali.1@ens.etsmtl.ca>
2026-09-22 16:28:06 -0400

    Setup spring boot

---
+++ src/main/java/com/azwa/azwa/AzwaApplication.java
@@ -0,0 +1,13 @@
+package com.azwa.azwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azwa/azwa/AzwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azwa.azwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
